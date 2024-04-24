6076e0dc727ec34aab056c9927f11e2b955d5316
DavidLudington <diludington@gmail.com>
2024-04-24 12:16:01 -0500

    src

---
+++ project1b/src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
@@ -0,0 +1,122 @@
+package edu.luc.cs271.arrayqueue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class FixedArrayQueue<E> implements SimpleQueue<E> {
+
+  private final int capacity;
+
+  private int size;
+
+  private int front;
+
+  private int rear;
+
+  private final E[] data;
+
+  // TODO why do we need an explicit constructor?
+
+  @SuppressWarnings("unchecked")
+  public FixedArrayQueue(final int capacity) {
+    // TODO check argument validity
+    if(capacity < 0){
+      throw new IllegalArgumentException("Capacity cannot be less that zero");
+    }
+    this.capacity = capacity;
+    this.data = (E[]) new Object[capacity];
+    this.size = 0;
+    this.front = 0;
+    this.rear = capacity - 1;
+  }
+
+  @Override
+  public boolean offer(final E obj) {
+    // TODO
+
+    if(isFull()){
+      return false;
+    } else{
+      rear = (rear + 1) % capacity;
+      data[rear] = obj;
+      size++;
+    }
+    return true;
+  }
+
+  @Override
+  public E peek() {
+    // TODO
+    if(isEmpty()){
+      return null;
+    } else{
+      return data[front];
+    } 
+    
+  }
+
+  @Override
+  public E poll() {
+    // TODO
+
+    if(isEmpty()){
+      return null;
+    } else {
+      E element = data[front];
+    front = (front + 1) % capacity;
+    size--;
+    return element;
+    }
+  }
+
+  @Override
+  public boolean isEmpty() {
+    // TODO
+    return size == 0;
+    
+  }
+
+  @Override
+  public boolean isFull() {
+    // TODO
+    return (size == capacity);
+    
+    /*
+    if((front == 0 && rear == size - 1) || (rear == (front - 1) % (size - 1))){
+      return true;
+    }
+    return false;
+    */
+    
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  public int capacity() {
+    return capacity;
+  }
+
+  @Override
+  public List<E> asList() {
+    // TODO implement using an ArrayList preallocated with the right size
+    final ArrayList<E> result = new ArrayList<>(size);
+    int idx = front;
+    for(int i = 0; i < size; i++){
+      result.add(data[idx]);
+      idx = (idx + 1) % capacity;
+    }
+    
+    return result;
+  }
+
+  @Override
+  public void clear(){
+    rear = front = size = 0;
+    Arrays.fill(data, null);
+  }
+}
---
+++ project1b/src/test/java/edu/luc/cs271/arrayqueue/TestSimpleQueueJqwik.java
@@ -0,0 +1,120 @@
+package edu.luc.cs271.arrayqueue;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+import net.jqwik.api.state.*;
+
+
+class TestSimpleQueueJqwik {
+
+  class OfferAction implements Action.Independent<SimpleQueue<String>> {
+    @Override
+    public boolean precondition(final SimpleQueue<String> queue) {
+      // TODO implement precondition for offer method
+      return !queue.isFull() && queue.capacity() > 0;
+    }
+    @Override
+    public Arbitrary<Transformer<SimpleQueue<String>>> transformer() {
+      final var offerElements = Arbitraries.strings().alpha().ofLength(5);
+      return offerElements.map(element -> Transformer.mutate(
+        String.format("offer(%s)", element),
+        queue -> {
+          // TODO capture state before offer, perform, and check postcondition
+          final var sizeBefore = queue.size();
+          final var peekBefore = queue.peek();
+          assertTrue(queue.offer(element));
+          assertFalse(queue.isEmpty());
+          assertEquals(sizeBefore + 1, queue.size());
+        }
+      ));
+    }
+  }
+
+  private Action<SimpleQueue<String>> poll() {
+    return Action.<SimpleQueue<String>>builder()
+      .describeAs("poll")
+      .justMutate(queue -> {
+        // TODO capture state before poll, perform, and check postcondition
+        final var sizeBefore = queue.size();
+        final var peek = queue.peek();
+        final var poll = queue.poll();
+        assertEquals(poll, peek);
+        if(sizeBefore >= 1){
+          assertEquals(queue.size(), sizeBefore - 1);
+        }
+        
+      });
+  }
+  
+  
+  class clearTest implements Action.Independent<SimpleQueue<String>> { 
+    @Override
+    public boolean precondition(final SimpleQueue<String> queue) {
+      // TODO implement precondition for offer method
+      return !queue.isFull() && queue.capacity() > 0;
+    }
+    @Override
+    public Arbitrary<Transformer<SimpleQueue<String>>> transformer() {
+      final var offerElements = Arbitraries.strings().alpha().ofLength(5);
+      return offerElements.map(element -> Transformer.mutate(
+        String.format("offer(%s)", element),
+        queue -> {
+          assertTrue(queue.offer(element));
+          assertFalse(queue.isEmpty());
+          queue.clear();
+          assertEquals(0, queue.size());
+          assertTrue(queue.isEmpty());
+        }
+      ));
+    }
+  }
+      
+
+  
+
+  
+  // TODO extra credit: apply property to different queue capacities
+  @Property
+  void checkSimpleQueue(@ForAll("simpleQueueActions") final ActionChain<SimpleQueue<String>> chain) {
+    // TODO insert observable data invariant(s) for 0 <= size <= capacity
+    chain
+    .withInvariant("size", queue -> assertTrue(0 <= queue.size() && queue.size() <= queue.capacity()))
+
+    .run();
+  }
+
+  @Property
+  void checkSimpleQueueDifferntSize(@ForAll("simpleQueueActionsCapacity3") final ActionChain<SimpleQueue<String>> chain) {
+    // TODO insert observable data invariant(s) for 0 <= size <= capacity
+    chain
+    .withInvariant("size", queue -> assertTrue(0 <= queue.size() && queue.size() <= queue.capacity()))
+    
+    .run();
+  }
+
+  @Provide
+  Arbitrary<ActionChain<SimpleQueue<String>>> simpleQueueActions() {
+    return ActionChain
+      .<SimpleQueue<String>>startWith(() -> new FixedArrayQueue<String>(5))
+      .withAction(new OfferAction())
+      .withAction(poll())
+      .withAction(new clearTest());
+
+  }
+
+  @Provide
+  Arbitrary<ActionChain<SimpleQueue<String>>> simpleQueueActionsCapacity3() {
+    return ActionChain
+      .<SimpleQueue<String>>startWith(() -> new FixedArrayQueue<String>(3))
+      .withAction(new OfferAction())
+      .withAction(poll())
+      .withAction(new clearTest());
+
+  }
+  
+}
+
